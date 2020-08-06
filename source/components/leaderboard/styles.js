abf02cb242eccae072c21058981c2a7cf4de2d70
Rowan Hogan <rowan.hogan@gmail.com>
2020-08-06 10:37:56 +1000

    Leaderboard columns using flex and order

--- source/components/leaderboard/styles.js
+++ source/components/leaderboard/styles.js
@@ -1,19 +1,51 @@
 import merge from 'lodash/merge'
+import range from 'lodash/range'
+
+const calculateOrder = (columnCount, columnSize, index) => {
+  const gridSize = columnCount * columnSize
+  const currentColumn = Math.ceil((index / gridSize) * columnCount)
+
+  return (
+    (index - (currentColumn * columnSize - columnSize)) * columnCount +
+    currentColumn -
+    columnCount
+  )
+}
 
 export default (
-  { background, foreground, columns, styles },
+  { background, children, foreground, columns, styles },
   { colors, mediaQuery, scale, rhythm, justifyContent, treatments }
 ) => {
+  const childrenCount = children ? children.length : 0
   const createColumns = () => {
-    return Object.keys(columns).reduce(
-      (styles, breakpoint) => ({
+    return Object.keys(columns).reduce((styles, breakpoint) => {
+      const columnCount = columns[breakpoint]
+      const columnSize = Math.ceil(childrenCount / columnCount)
+
+      return {
         ...styles,
-        [mediaQuery(breakpoint)]: {
-          columnCount: columns[breakpoint]
-        }
-      }),
-      {}
-    )
+        [mediaQuery(breakpoint)]:
+          childrenCount > 1
+            ? {
+              display: 'flex',
+              flexWrap: 'wrap',
+              '> .c11n-leaderboard-item': {
+                width: `${100 / columnCount}%`,
+                ...range(1, childrenCount + 1).reduce((acc, index) => {
+                  return {
+                    ...acc,
+                    [`&:nth-of-type(${index})`]: {
+                      order: calculateOrder(columnCount, columnSize, index)
+                    }
+                  }
+                }, {})
+              }
+            }
+            : {
+              columnCount
+            }
+      }
+    }, {})
   }
 
   const defaultStyles = {
